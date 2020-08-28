f5ee9fb64842adf6ec47bc04aec7cb6cecb79bb1
eslidiingz <eslidiingz@gmail.com>
2020-08-28 17:28:00 +0700

    fix migrate form_borrows

--- database/migrations/2020_08_28_135847_create_form_borrows_table.php
+++ database/migrations/2020_08_28_135847_create_form_borrows_table.php
@@ -15,13 +15,13 @@ class CreateFormBorrowsTable extends Migration
     {
         Schema::create('form_borrows', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id')->nullable()->->comment('รหัสผู้ใช้');
-            $table->string('write_at')->nullable()->->comment('เขียนที่');
-            $table->unsignedBigInteger('copy_card')->nullable()->->comment('สำเนาบัตรประจำตัวคนพิการ');
-            $table->unsignedBigInteger('house_res')->nullable()->->comment('สำเนาทะเบียนบ้านคนพิการ');
-            $table->unsignedBigInteger('copy_train')->nullable()->->comment('สำเนารับรองการเข้าฝึกอบรม');
-            $table->unsignedBigInteger('sub_copy_citizen_id')->nullable()->->comment('สำเนาบัตรประชาชน ,สำเนาทะเบียนบ้าน');
-            $table->unsignedBigInteger('power_attorney')->nullable()->->comment('หนังสือมอบอำนาจ');
+            $table->unsignedBigInteger('user_id')->nullable()->comment('รหัสผู้ใช้');
+            $table->string('write_at')->nullable()->comment('เขียนที่');
+            $table->unsignedBigInteger('copy_card')->nullable()->comment('สำเนาบัตรประจำตัวคนพิการ');
+            $table->unsignedBigInteger('house_res')->nullable()->comment('สำเนาทะเบียนบ้านคนพิการ');
+            $table->unsignedBigInteger('copy_train')->nullable()->comment('สำเนารับรองการเข้าฝึกอบรม');
+            $table->unsignedBigInteger('sub_copy_citizen_id')->nullable()->comment('สำเนาบัตรประชาชน ,สำเนาทะเบียนบ้าน');
+            $table->unsignedBigInteger('power_attorney')->nullable()->comment('หนังสือมอบอำนาจ');
             $table->enum('type', ['1', '2'])
                 ->default('1')
                 ->comment('ประเภท 1 คนพิการ, 2 ผู้ยื่นคำขอแทน');
