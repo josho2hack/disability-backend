2651b5124ae4683031095635b042be7ca6f5f5a5
aritad thipchak <aonaon1159@gmail.com>
2020-09-03 12:31:48 +0700

    update pratice page

--- app/Http/Controllers/PracticeController.php
+++ app/Http/Controllers/PracticeController.php
@@ -18,7 +18,33 @@ class PracticeController extends Controller
     }
 
     public function add(Request $request){
-    	
+
     	return redirect('practice')->with('message', 'ลงทะเบียนเรียบร้อยแล้ว');
+    }
+
+
+
+    public function getsubgroup(Request $request)
+    {
+    	$sg = SubGroup::where('main_groups_id',$request->main_id)->get();
+		$subgroup = "<option value=''>- เลือก -</option>";
+		
+		foreach( $sg as $sub ){
+			$subgroup .= "<option value='{$sub->id}'>{$sub->name}</option>";
+		}
+
+    	return $subgroup;
+    }
+
+    public function getcategory(Request $request)
+    {
+    	$category = SubGroup::where('main_groups_id',$request->sub_id)->get();
+		$categories = "<option value=''>- เลือก -</option>";
+		
+		foreach( $category as $cate ){
+			$categories .= "<option value='{$cate->id}'>{$cate->name}</option>";
+		}
+
+    	return $categories;
     }
 }
